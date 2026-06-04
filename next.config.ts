592ffec29ce09168bc248c7ae0b8bd55987525c1
Антон Ковальчук <antonkovalcuk@MacBook-Pro-Home.local>
2026-06-04 12:13:49 +0300

    Fix basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: '/sber-dashboard',
+  assetPrefix: '/sber-dashboard',
 };
 
 export default nextConfig;
